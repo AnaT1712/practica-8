9db5150f1aff8a983810c3927ec8240a699ef3a1
AnaT1712 <anathernandeza1@gmail.com>
2025-04-03 14:05:37 -0600

    Update ejer1 (while & do while).c

--- ejer1 (while & do while).c
+++ ejer1 (while & do while).c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+// ejercicio en while
 
 int main()
 {
@@ -23,6 +24,41 @@ int main()
         
         i=i+1;
     }
+    
+    printf("el ultimo numero par es: %d\n", p);
+    printf("el ultimo numero impar es: %d\n", imp);
+    
+    return 0;
+}
+
+//ejercicio en do while
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i=0;
+    int p=0;
+    int imp=0;
+    
+    printf("dame un numero:");
+    scanf("%d", &n);
+    
+    do
+    {
+        if (i%2==0)
+        {
+            p= i;
+        }
+        else 
+        {
+            imp= i;
+        }
+        
+        i=i+1;
+    }
+    while (i<n);
     
     printf("el ultimo numero par es: %d\n", p);
     printf("el ultimo numero impar es: %d\n", imp);
